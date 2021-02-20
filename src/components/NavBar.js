6e950b3ce123087881cc5400c247d8d0483cc18d
camimgh <josephinecmejia@gmail.com>
2021-02-20 15:54:01 -0500

    edited navbar and added firebase

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,13 @@ class NavBar extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">aligned</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/resources">Resources</Nav.Link>
+                <Nav.Link href="/submitreview">Write a Review</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
