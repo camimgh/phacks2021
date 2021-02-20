f9d0af0d706665396110695965091b56a94de129
camimgh <josephinecmejia@gmail.com>
2021-02-20 15:27:31 -0500

    added landing page component

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './LandingPage.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <p>hello</p>
+        )
+    }
+}
+
+export default LandingPage;
